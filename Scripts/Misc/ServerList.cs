d54e651fdffc09dba8e26cce68b524aff39fb06e
Zilmerx <hex404@gmail.com>
2017-04-06 14:28:55 -0400

    MaJ le nom du serveur, "." avant les commandes.

--- Scripts/Misc/ServerList.cs
+++ Scripts/Misc/ServerList.cs
@@ -39,7 +39,7 @@ namespace Server.Misc
 		 */
 
 		public static readonly string Address = null;
-		public static readonly string ServerName = "RunUO TC";
+		public static readonly string ServerName = "Zystria";
 
 		public static readonly bool AutoDetect = true;
 
